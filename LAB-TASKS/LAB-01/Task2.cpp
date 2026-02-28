105c1c5c85430122c844ffbc98895448563f5947
khabsarali <khabsarali@gmail.com>
2026-02-28 22:31:13 +0500

    message

--- LAB-TASKS/LAB-01/Task2.cpp
+++ LAB-TASKS/LAB-01/Task2.cpp
@@ -8,68 +8,38 @@ struct Student {
     float marks;
 
     void displayStudentInfo() {
-        cout << "Full Name: " << firstName << " " << lastName << endl;
-        cout << "Roll Number: " << rollNumber << endl;
+        cout << "\nFull Name: " << firstName << " " << lastName << endl;
         cout << "Marks: " << marks << endl;
-        cout << "-------------------------" << endl;
     }
 };
 
 int main() {
-    int choice;
+    int n;
 
-    cout << "=== Student Management Program ===\n";
-    cout << "1. Enter multiple students (array of structures)\n";
-    cout << "2. Enter single student (pointer to structure)\n";
-    cout << "Enter your choice: ";
-    cin >> choice;
+    cout << "Enter number of students: ";
+    cin >> n;
 
-    if (choice == 1) {
-        // Array of structures
-        int n;
-        cout << "\nEnter number of students: ";
-        cin >> n;
+    Student students[n];  // array of structures
 
-        Student students[n];  // Array
+    for(int i = 0; i < n; i++) {
+        cout << "\nEnter details for student " << i+1 << endl;
 
-        for (int i = 0; i < n; i++) {
-            cout << "\nEnter details for Student " << i + 1 << endl;
-            cout << "First Name: ";
-            cin >> students[i].firstName;
-            cout << "Last Name: ";
-            cin >> students[i].lastName;
-            cout << "Roll Number: ";
-            cin >> students[i].rollNumber;
-            cout << "Marks: ";
-            cin >> students[i].marks;
-        }
-
-        cout << "\n--- Student Information ---\n";
-        for (int i = 0; i < n; i++) {
-            students[i].displayStudentInfo();
-        }
-
-    } else if (choice == 2) {
-        // Pointer to structure
-        Student *ptr = new Student;
-
-        cout << "\nEnter details for the student:\n";
         cout << "First Name: ";
-        cin >> ptr->firstName;
+        cin >> students[i].firstName;
+
         cout << "Last Name: ";
-        cin >> ptr->lastName;
+        cin >> students[i].lastName;
+
         cout << "Roll Number: ";
-        cin >> ptr->rollNumber;
+        cin >> students[i].rollNumber;
+
         cout << "Marks: ";
-        cin >> ptr->marks;
+        cin >> students[i].marks;
+    }
 
-        cout << "\n--- Student Information ---\n";
-        ptr->displayStudentInfo();
-
-        delete ptr;  // Free memory
-
-    } else {
-        cout << "Invalid choice!\n";
+    // Display all students
+    for(int i = 0; i < n; i++) {
+        students[i].displayStudentInfo();
     }
 
     return 0;
